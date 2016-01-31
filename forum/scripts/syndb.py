8d9dca2e905c0f09f908f80f5006426bd531f7c0
guillemp <guillemzz@gmail.com>
2016-01-31 12:29:06 +0100

    Added check forum integrity

--- forum/scripts/syndb.py
+++ forum/scripts/syndb.py
@@ -1,149 +1,41 @@
-from django.db.models import Max, Count
 from main.models import Forum, Topic, Post
 
 # all forums
 forums = Forum.objects.all()
 
+# check last post id
 for forum in forums:
-    last_post_id = Post.objects.filter(forum=forum).aggregate(Max('id'))
-    total_posts = Post.objects.filter(forum=forum).count()
-    print last_post_id, total_posts
+    last_post = Post.objects.filter(forum=forum).order_by('-id')[0]
+    print forum.id, forum.last_post.id, last_post.id
 
+# check posts count
+for forum in forums:
+    total_posts = Post.objects.filter(forum=forum).count()
+    print forum.id, forum.posts, total_posts
+
+# check topics count
+for forum in forums:
+    total_topics = Topic.objects.filter(forum=forum).count()
+    print forum.id, forum.topics, total_topics
 
 # all topics
+topics = Topic.objects.all()
 
+# check first post id
+for topic in topics:
+    first_post = Post.objects.filter(topic=topic).order_by('id')[0]
+    if topic.first_post.id != first_post.id:
+        print topic.id, topic.first_post.id, first_post.id
 
+# check last post id
+for topic in topics:
+    last_post = Post.objects.filter(topic=topic).order_by('-id')[0]
+    if topic.last_post.id != last_post.id:
+        print topic.id, topic.last_post.id, last_post.id
 
+# check replies count
+for topic in topics:
+    replies = Post.objects.filter(topic=topic).count() - 1
+    if topic.replies != replies:
+        print topic.id, topic.replies, replies
 
-'''
-function sync($type, $id = false)
-{
-	global $db;
-
-	switch($type)
-	{
-		case 'all forums':
-			$sql = "SELECT forum_id
-				FROM " . FORUMS_TABLE;
-			if ( !($result = $db->sql_query($sql)) )
-			{
-				message_die(GENERAL_ERROR, 'Could not get forum IDs', '', __LINE__, __FILE__, $sql);
-			}
-
-			while( $row = $db->sql_fetchrow($result) )
-			{
-				sync('forum', $row['forum_id']);
-			}
-		   	break;
-
-		case 'all topics':
-			$sql = "SELECT topic_id
-				FROM " . TOPICS_TABLE;
-			if ( !($result = $db->sql_query($sql)) )
-			{
-				message_die(GENERAL_ERROR, 'Could not get topic ID', '', __LINE__, __FILE__, $sql);
-			}
-
-			while( $row = $db->sql_fetchrow($result) )
-			{
-				sync('topic', $row['topic_id']);
-			}
-			break;
-
-	  	case 'forum':
-			$sql = "SELECT MAX(post_id) AS last_post, COUNT(post_id) AS total 
-				FROM " . POSTS_TABLE . "  
-				WHERE forum_id = $id";
-			if ( !($result = $db->sql_query($sql)) )
-			{
-				message_die(GENERAL_ERROR, 'Could not get post ID', '', __LINE__, __FILE__, $sql);
-			}
-
-			if ( $row = $db->sql_fetchrow($result) )
-			{
-				$last_post = ( $row['last_post'] ) ? $row['last_post'] : 0;
-				$total_posts = ($row['total']) ? $row['total'] : 0;
-			}
-			else
-			{
-				$last_post = 0;
-				$total_posts = 0;
-			}
-
-			$sql = "SELECT COUNT(topic_id) AS total
-				FROM " . TOPICS_TABLE . "
-				WHERE forum_id = $id";
-			if ( !($result = $db->sql_query($sql)) )
-			{
-				message_die(GENERAL_ERROR, 'Could not get topic count', '', __LINE__, __FILE__, $sql);
-			}
-
-			$total_topics = ( $row = $db->sql_fetchrow($result) ) ? ( ( $row['total'] ) ? $row['total'] : 0 ) : 0;
-
-			$sql = "UPDATE " . FORUMS_TABLE . "
-				SET forum_last_post_id = $last_post, forum_posts = $total_posts, forum_topics = $total_topics
-				WHERE forum_id = $id";
-			if ( !$db->sql_query($sql) )
-			{
-				message_die(GENERAL_ERROR, 'Could not update forum', '', __LINE__, __FILE__, $sql);
-			}
-			break;
-
-		case 'topic':
-			$sql = "SELECT MAX(post_id) AS last_post, MIN(post_id) AS first_post, COUNT(post_id) AS total_posts
-				FROM " . POSTS_TABLE . "
-				WHERE topic_id = $id";
-			if ( !($result = $db->sql_query($sql)) )
-			{
-				message_die(GENERAL_ERROR, 'Could not get post ID', '', __LINE__, __FILE__, $sql);
-			}
-
-			if ( $row = $db->sql_fetchrow($result) )
-			{
-				if ($row['total_posts'])
-				{
-					// Correct the details of this topic
-					$sql = 'UPDATE ' . TOPICS_TABLE . ' 
-						SET topic_replies = ' . ($row['total_posts'] - 1) . ', topic_first_post_id = ' . $row['first_post'] . ', topic_last_post_id = ' . $row['last_post'] . "
-						WHERE topic_id = $id";
-
-					if (!$db->sql_query($sql))
-					{
-						message_die(GENERAL_ERROR, 'Could not update topic', '', __LINE__, __FILE__, $sql);
-					}
-				}
-				else
-				{
-					// There are no replies to this topic
-					// Check if it is a move stub
-					$sql = 'SELECT topic_moved_id 
-						FROM ' . TOPICS_TABLE . " 
-						WHERE topic_id = $id";
-
-					if (!($result = $db->sql_query($sql)))
-					{
-						message_die(GENERAL_ERROR, 'Could not get topic ID', '', __LINE__, __FILE__, $sql);
-					}
-
-					if ($row = $db->sql_fetchrow($result))
-					{
-						if (!$row['topic_moved_id'])
-						{
-							$sql = 'DELETE FROM ' . TOPICS_TABLE . " WHERE topic_id = $id";
-			
-							if (!$db->sql_query($sql))
-							{
-								message_die(GENERAL_ERROR, 'Could not remove topic', '', __LINE__, __FILE__, $sql);
-							}
-						}
-					}
-
-					$db->sql_freeresult($result);
-				}
-			}
-			break;
-	}
-	
-	return true;
-}
-'''
